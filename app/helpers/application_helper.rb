b1f4acc9029ac19b4294de5527fc0ebebd112217
Tiago Godinho <tiagogodinho3@gmail.com>
2009-02-20 15:28:42 -0300

    alteracao do layout da pagina que agora conta com exibixao da foto atraves do site gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def gravatar_url_for(email, options = {})
+    url_for({
+      :gravatar_id => Digest::MD5.hexdigest(email),
+      :host => 'www.gravatar.com',
+      :protocol => 'http://',
+      :only_path => false,
+      :controller => 'avatar.php'}.merge(options))
+  end
 end
