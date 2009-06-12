455d1ff4718b3650b4251444715219d8d74f8c5e
Tiago Godinho <tiagogodinho3@gmail.com>
2009-06-11 22:25:48 -0300

    indentacao do codigo

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -1,53 +1,52 @@
 class TarefasController < ApplicationController
-
   layout "principal"
   before_filter :create_menu, :login_required
-
+  
   # GET /tarefas
   # GET /tarefas.xml
   def index
-  	@tarefas = @projeto.tarefas
+    @tarefas = @projeto.tarefas
     #@tarefas = @projeto.tarefas.all(:order => "DtTermino desc")
     
     respond_to do |format|
-        format.html # index.html.erb
-        format.xml  { render :xml => @tarefas }
+      format.html # index.html.erb
+      format.xml  { render :xml => @tarefas }
     end
   end
-
+  
   # GET /tarefas/1
   # GET /tarefas/1.xml
   def show
     @tarefa = Tarefa.find(params[:id])
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @tarefa }
     end
   end
-
+  
   # GET /tarefas/new
   # GET /tarefas/new.xml
   def new
-	@tarefa = Tarefa.new
-
-	respond_to do |format|
-	  format.html # new.html.erb
-	  format.xml  { render :xml => @tarefa }
-	end
+    @tarefa = Tarefa.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @tarefa }
+    end
   end
-
+  
   # GET /tarefas/1/edit
   def edit
-	@tarefa = Tarefa.find(params[:id])
+    @tarefa = Tarefa.find(params[:id])
   end
-
+  
   # POST /tarefas
   # POST /tarefas.xml
   def create
     @tarefa = Tarefa.new(params[:tarefa])
     @tarefa.projeto_id = @projeto.id
-      
+    
     respond_to do |format|
       if @tarefa.save
         flash[:notice] = 'Tarefa was successfully created.'
@@ -59,41 +58,40 @@ class TarefasController < ApplicationController
       end
     end
   end
-
+  
   # PUT /tarefas/1
   # PUT /tarefas/1.xml
   def update
-	@tarefa = Tarefa.find(params[:id])
-
-	respond_to do |format|
-	  if @tarefa.update_attributes(params[:tarefa])
-		flash[:notice] = 'Tarefa was successfully updated.'
-		format.html { redirect_to(projeto_tarefa_path(@projeto, @tarefa)) }
-		format.xml  { head :ok }
-	  else
-		format.html { render :action => "edit" }
-		format.xml  { render :xml => @tarefa.errors, :status => :unprocessable_entity }
-	  end
-	end
+    @tarefa = Tarefa.find(params[:id])
+    
+    respond_to do |format|
+      if @tarefa.update_attributes(params[:tarefa])
+        flash[:notice] = 'Tarefa was successfully updated.'
+        format.html { redirect_to(projeto_tarefa_path(@projeto, @tarefa)) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @tarefa.errors, :status => :unprocessable_entity }
+      end
+    end
   end
-
+  
   # DELETE /tarefas/1
   # DELETE /tarefas/1.xml
   def destroy
-	@tarefa = Tarefa.find(params[:id])
-	@tarefa.destroy
-
-	respond_to do |format|
-	  format.html { redirect_to(projeto_tarefas_path(@projeto)) }
-	  format.xml  { head :ok }
-	end
+    @tarefa = Tarefa.find(params[:id])
+    @tarefa.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to(projeto_tarefas_path(@projeto)) }
+      format.xml  { head :ok }
+    end
   end
   
   def create_menu
-	@projetos = Projeto.find(:all)
-	@projeto = Projeto.find(params[:projeto_id])
-	@id = params[:projeto_id]
-	@aba = :tarefas
+    @projetos = Projeto.find(:all)
+    @projeto = Projeto.find(params[:projeto_id])
+    @id = params[:projeto_id]
+    @aba = :tarefas
   end
-
 end
