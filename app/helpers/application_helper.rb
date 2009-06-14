45ab118b3ee81969ecd7f606a0428f4059500bcf
Tiago Godinho <tiagogodinho3@gmail.com>
2009-06-14 17:45:00 -0300

    mudanca do metodo para adaptacao ao servico do Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def gravatar_url_for(email, options = {})
-    url_for({
-      :gravatar_id => Digest::MD5.hexdigest(email),
-      :host => 'secure.gravatar.com',
-      :protocol => 'https://',
-      :only_path => false,
-      :controller => 'avatar.php'}.merge(options))
+    return "http://www.gravatar.com/avatar/" + Digest::MD5.hexdigest(email) + ".jpg"
   end
   
   def pessoa_projetos
