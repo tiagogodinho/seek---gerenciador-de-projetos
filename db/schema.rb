8afadbaee54f2fa33035a83871a44e1677370e70
Tiago Godinho <tiagogodinho3@gmail.com>
2009-06-21 23:11:41 -0300

    atualizacao do shema do banco de dados

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090615004615) do
+ActiveRecord::Schema.define(:version => 20090622020550) do
 
   create_table "comments", :force => true do |t|
     t.text     "comment"
@@ -17,6 +17,10 @@ ActiveRecord::Schema.define(:version => 20090615004615) do
     t.integer  "tarefa_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "participantes", :force => true do |t|
